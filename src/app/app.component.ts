b282d3696c1bb287da508661fa3a8d7308409817
santosh raju vysyaraju <santhoshraju621@gmail.com>
2022-02-19 17:16:18 +0530

    updated the code for new repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UpdateTimeComponent } from './dialogs/update-time/update-time.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'novatek-assignment';
+  constructor(private dialog: MatDialog) {}
+
+  openDialog() {
+   this.dialog.open(UpdateTimeComponent, {
+    hasBackdrop: true
+   }); 
+  }
 }
